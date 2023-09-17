12ae5a4057dd9a2f4aae0109a73bee39a70ee1c0
简静凡 <30424139+wtto00@users.noreply.github.com>
2023-09-17 22:29:01 +0800

    chore: test ci

--- spec/afterStart.test.ts
+++ spec/afterStart.test.ts
@@ -21,7 +21,7 @@ beforeAll(async () => {
   if (emulatorId) {
     await android.ensureReady(emulatorId);
   }
-}, 120000);
+}, 300000);
 
 afterAll(async () => {
   await android.waitForStop(emulatorId);
